619537c0259d2f34abe97229138c133d82e1567b
EuanR-D <erenfreydouglas@gmail.com>
2025-09-24 16:49:15 +1000

    Q3.1 Define strings
    Added 4 strings and logged to console, tested with node.js

--- Part 3/strings.js
+++ Part 3/strings.js
@@ -1,2 +1,14 @@
 // String Manipulation and files
 
+// Q3.1
+// Defining 4 string variables
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Euan";
+
+// Logging all strings to console to test
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
