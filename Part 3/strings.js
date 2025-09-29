8b44d4ce330f7a039af0d134103377b76fde5268
EuanR-D <erenfreydouglas@gmail.com>
2025-09-30 08:33:46 +1000

    Q3.2 String util functions
    Using length and charAt functions on strings

--- Part 3/strings.js
+++ Part 3/strings.js
@@ -12,3 +12,16 @@ console.log(myString);
 console.log(anotherString);
 console.log(hello);
 console.log(myName);
+
+
+// Q3.2
+
+// outputting length of myString to console
+console.log(`myString is ${myString.length} char long`);
+
+// outputting first character of myString to console
+console.log(`1st char of myString is ${myString.charAt(0)}`);
+
+// outputting 11th character of myString to console
+console.log(`11th char of myString is ${myString.charAt(10)}`);
+
