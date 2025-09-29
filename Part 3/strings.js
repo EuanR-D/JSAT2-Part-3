94f88f319aab9fc710a1eb736d1dc3fc3d759cf8
EuanR-D <erenfreydouglas@gmail.com>
2025-09-30 09:58:51 +1000

    Q3.6 File algorithms

--- Part 3/strings.js
+++ Part 3/strings.js
@@ -62,3 +62,49 @@ console.log(replacedString);
 // turning myString into an array by splitting at the spaces
 let splitString = myString.split(" ");
 console.log(splitString);
+
+
+// Q3.6
+// Importing node file system module
+const fs = require('node:fs');
+
+// write to file function
+// 2 params for text and name of file
+function writeToFile(data, fileName) {
+  console.log("Writing to file..");
+
+  // creates file with name given, writes data to said file
+  fs.writeFile(`${fileName}`, data, (err) => {
+
+    if (err) {
+      // outputs error message if something fails
+      console.error(err);
+
+    } else {
+      // output message on successful execution
+      console.log("File successfully written.");
+    }
+  });
+}
+
+// read from file function
+// 1 param, the file to read from
+function readFromFile(fileName) {
+  console.log("Reading file..");
+
+  fs.readFile(`${fileName}`, (err, fileText) => {
+
+    if (err) {
+      // outputs error message and returns if something fails
+      console.error(err);
+      return;
+    }
+
+    // outputs the contents of read file as a string to console
+    console.log('File contents:');
+    console.log(fileText.toString());
+  });
+}
+
+
+
